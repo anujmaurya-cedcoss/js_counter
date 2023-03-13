00045586b19aeea2862c171f10283154c751fe06
Anuj Maurya <anujmaurya@cedcoss.com>
2023-03-13 13:02:59 +0530

    modified a few conditions to check if entered number is valid

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,27 +1,30 @@
 // alert for no number
-function noNum() {
+function noNumAlert() {
     alert("Please enter a number");
 }
 // function to check if no number has been entered
 function nan(num) {
     return num.length;
 }
+// function to increment
 function plus() {
     let num = document.getElementById("input-box").value;
-    if(!nan(num)){
-        noNum();
+    if (!nan(num)) {
+        noNumAlert();
+    } else if(num < 0) {
+        alert("Please enter a positive number");   
     } else {
         num++;
         document.getElementById("input-box").value = num;
-    } 
-    
-}
+    }
 
+}
+// function to decrement
 function minus() {
     let num = document.getElementById("input-box").value;
-    if(!nan(num)) {
-        noNum();
-    }else if(num <= 0) {
+    if (!nan(num)) {
+        noNumAlert();
+    } else if (num <= 0) {
         alert("Can't go below 0");
     } else {
         num--;
